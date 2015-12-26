d043cd14d098d310b70964b7edd1f7ca6a9190e1
cheng <453027119@qq.com>
2015-12-27 02:20:58 +0800

    init1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var main=document.querySelector('#main');
 var oLis=document.querySelectorAll('.slide>li');
 var winW=window.innerWidth;
@@ -51,4 +52,70 @@ function end(e){
         this.style.webkitTransition="";
         this.firstElementChild.id="a"+(this.index+1);
     });
+}
+*/
+var main = document.querySelector('#main');
+var oLis = document.querySelectorAll(".slide>li");
+var winW = window.innerWidth;
+var winH = window.innerHeight;//设备的高度
+var desW = 640;
+var desH = 960;//设计高的高度
+if(desW/desH<winW/winH){
+    main.style.webkitTransform = 'scale('+winW/desW+')';
+}else{
+    main.style.webkitTransform = 'scale('+winH/desH+')';
+}
+[].forEach.call(oLis, function () {
+    console.log(arguments);
+    arguments[0].index = arguments[1];
+    arguments[0].addEventListener('touchstart', start, false);
+    arguments[0].addEventListener('touchmove', move, false);
+    arguments[0].addEventListener('touchend', end, false);
+});
+function start(e) {
+    this.startY = e.changedTouches[0].pageY;
+    console.log(this.startY);
+    console.log(e.changedTouches[0]);
+    console.log(e.touches);
+    console.log(e.changedTouches);
+    console.log(e.targetTouches);
+}
+function move(e) {
+    e.preventDefault();
+    /*阻止默认行为*/
+    var touchMove = e.changedTouches[0].pageY;
+    console.log("112",touchMove);
+    var changePos = touchMove - this.startY;
+    console.log("111",changePos);
+    var cur = this.index;
+    var step = 1/2;
+    var scalePos =(Math.abs(changePos)/winH)*step;
+    [].forEach.call(oLis,function(){
+        if(arguments[1]!=cur){
+            arguments[0].style.display="none";
+        }
+        arguments[0].className="";
+        arguments[0].firstElementChild.id="";
+    })
+    if (changePos > 0) {/*↓*/
+        var pos = -winH+changePos;
+        this.preSIndex = cur == 0 ? oLis.length - 1 : cur - 1;
+
+    } else if (changePos < 0) {/*↑*/
+        var pos = winH+changePos;
+        this.preSIndex = cur == oLis.length - 1 ? 0 : cur + 1;
+
+    }
+    oLis[this.preSIndex].style.webkitTransform = "translate(0,"+pos+"px)";
+    oLis[this.preSIndex].className = "zIndex";
+    oLis[this.preSIndex].style.display="block";
+    oLis[cur].style.webkitTransform = "scale("+(1-scalePos)+") translate(0,"+changePos+"px) ";
+}
+function end(e) {
+    oLis[this.preSIndex].style.webkitTransform ="translate(0,0)";
+    oLis[this.preSIndex].style.webkitTransition="0.5s";
+    oLis[this.preSIndex].addEventListener('webkitTransitionEnd',function(){
+        this.style.webkitTransition="";
+        this.firstElementChild.id = "a"+(this.index+1);
+    })
 }
