2a564907f37a2de400adb59af051ed459e4d9d63
cheng <453027119@qq.com>
2015-12-27 02:11:40 +0800

    init1

--- index.js
+++ index.js
@@ -4,34 +4,33 @@ var winW=window.innerWidth;
 var winH=window.innerHeight;
 var desW=640;
 var desH=960;
-//main.style.webkitTransform="scale("+winH/desH +")";
 if(desW/desH<winW/winH){
     main.style.webkitTransform = 'scale('+winW/desW+')';
 }else{
     main.style.webkitTransform = 'scale('+winH/desH+')';
 }
+
 [].forEach.call(oLis,function(){
     arguments[0].index=arguments[1];
-    arguments[0].addEventListener('touchstart',start,false);
-    arguments[0].addEventListener('touchmove',move,false);
-    arguments[0].addEventListener('touchend',end,false);
+    arguments[0].addEventListener("touchstart",start,false);
+    arguments[0].addEventListener("touchmove",move,false);
+    arguments[0].addEventListener("touchend",end,false);
 });
 function start(e){
     this.startY= e.changedTouches[0].pageY;
 }
 function move(e){
-    e.preventDefault();
     var touchMove= e.changedTouches[0].pageY;
     var changePos=touchMove-this.startY;
     var cur=this.index;
     var step=1/2;
     var scalePos=(Math.abs(changePos)/winH)*step;
-    [].forEach.call(oLis,function(){
+    [].forEach.call(oLis, function () {
         if(arguments[1]!=cur){
             arguments[0].style.display="none";
         }
-        arguments[0].className='';
-        arguments[0].firstElementChild.id='';
+        arguments[0].className="";
+        arguments[0].firstElementChild.id="";
     });
     if(changePos>0){
         var pos=-winH+changePos;
@@ -40,16 +39,19 @@ function move(e){
         var pos=winH+changePos;
         this.preSIndex=cur===oLis.length-1?0:cur+1;
     }
-    oLis[this.preSIndex].style.webkitTransform="translate(0,"+ pos+"+px)";
+    oLis[this.preSIndex].style.webkitTransform="translate(0,"+pos+"px)";
     oLis[this.preSIndex].className="zIndex";
     oLis[this.preSIndex].style.display="block";
     oLis[cur].style.webkitTransform="scale("+(1-scalePos)+") translate(0,"+changePos+"px)";
 }
 function end(e){
     oLis[this.preSIndex].style.webkitTransform="translate(0,0)";
-  oLis[this.preSIndex].style.webkitTransition="0.5s";
-    oLis[this.preSIndex].addEventListener('webkitTransitionEnd',function(){
+    oLis[this.preSIndex].style.webkitTransition="0.5s";
+    oLis[this.preSIndex].addEventListener("webkitTransitionEnd",function(){
         this.style.webkitTransition="";
         this.firstElementChild.id="a"+(this.index+1);
-    })
+    });
 }
+document.addEventListener("touchmove", function () {
+
+});
